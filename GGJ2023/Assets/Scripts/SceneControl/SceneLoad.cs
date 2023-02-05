6f9c5fc73c70a0580ceb5160f0f90559e37aff69
Yinguno <eric0101@gmail.com>
2023-02-05 14:48:40 +0800

    [新增]遊戲開始、說明、關於和遊戲中的的UI畫面

--- GGJ2023/Assets/Scripts/SceneControl/SceneLoad.cs
+++ GGJ2023/Assets/Scripts/SceneControl/SceneLoad.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneLoad : MonoBehaviour
 {
-    void LoadScene()
+    public void LoadScene(int index)
     {
-        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+
+    }
+    public void Quit()
+    {
+        Debug.Log("Qiut");
+        UnityEngine.Application.Quit();
     }
 }
